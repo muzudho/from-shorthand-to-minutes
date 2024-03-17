f1d7f0e61fdb53b102422ec848cce3b8bc14fa47
むずでょ <muzudho1@gmail.com>
2024-03-17 15:35:17 +0900

    format

--- format_work.py
+++ format_work.py
@@ -11,21 +11,31 @@ def main():
 －－－－－－－－－－－－＋
 　　　　　　　　　　　　｜
 """)
+        previous_speaker = ""
 
         for record in document:
             speaker = record["speaker"]
             statement = record["statement"]
-
-            f_out.write(f"　　　　　　　　　 {speaker}　｜　")
-
             is_first = True
+            
             for statement_line in statement:
                 #print(f"(...) {json.dumps(statement_line, ensure_ascii=False)}")
+
+                # 発言者が変わる時、１行開ける
+                if is_first and speaker != "" and previous_speaker != speaker:
+                    print(f"(^q^) speaker：［{speaker}］　previous_speaker：［{previous_speaker}］　発言者が変わる時、１行開ける")
+                    f_out.write("　　　　　　　　　　　　｜\n")
+                else:
+                    print(f"(^q^) speaker：［{speaker}］　previous_speaker：［{previous_speaker}］　発言者同じ")
+
                 if is_first:
+                    #print("(^q^) 発言の１行目：［{statement_line}］")
                     is_first = False
-                    f_out.write(f"{statement_line}\n")
+                    f_out.write(f"　　　　　　　　　 {speaker}　｜　{statement_line}\n")
                 else:
                     f_out.write(f"　　　　　　　　　　　　｜　{statement_line}\n")
+
+            previous_speaker = speaker
 
         f_out.write("""\
 　　　　　　　　　　　　｜
