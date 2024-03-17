e23aee6afe8a35aed6c6a923c5efbc3ac64c6946
むずでょ <muzudho1@gmail.com>
2024-03-17 13:05:37 +0900

    write

--- shorthand_to_work.py
+++ shorthand_to_work.py
@@ -1,10 +1,14 @@
-# python read_shorthand.py
+# python shorthand_to_work.py
 
 def main():
     with open("input.txt", "r", encoding="UTF-8") as f:
         lines = f.readlines()
         for line in lines:
             print(f"（＾～＾） {line}")
+
+    with open("work.txt", "a", encoding="UTF-8") as f2:
+        for line in lines:
+            f2.write(f"（＾～＾） {line}")
 
 if __name__ == "__main__":
     main()
