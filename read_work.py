23e8ddf98ace30c88b42a0a93e10eb61e9fd3320
むずでょ <muzudho1@gmail.com>
2024-03-17 14:35:29 +0900

    空行対応

--- read_work.py
+++ read_work.py
@@ -4,9 +4,15 @@ import json
 def main():
 
     with open("work.json", "r", encoding="UTF-8") as f_in:
-        data = json.load(f_in)
+        document = json.load(f_in)
 
-        print(f"(^q^) {json.dumps(data)}")
+        for record in document:
+            speaker = record["speaker"]
+            print(f"(^q^) {speaker}")
+
+            statement = record["statement"]
+            for statement_line in statement:
+                print(f"(...) {json.dumps(statement_line)}")
 
 if __name__ == "__main__":
     main()
--- shorthand_to_work.py
+++ shorthand_to_work.py
@@ -10,19 +10,27 @@ def main():
         for line in lines:
             #print(f"（＾～＾） {line}")
 
+            # 末尾の改行は削除
             # めんどくさいんでタブはざっくりスペース 4つに変換
-            line = line.replace("\t", "    ")
+            line = line.rstrip().replace("\t", "    ")
 
+            # 空行はパス
+            if line == "":
+                pass
             # 発言者名と発言の行
-            if line.startswith(" "):
+            elif not line.startswith(" "):
+                print(f"（＾～＾）発言者名と発言の行　［{line}］")
                 pair = line.split(" ", maxsplit=2)
                 speaker = pair[0]
+                print(f"（＾～＾）発言者名　［{speaker}］")
                 # 改行は \r\n と仮定
-                statement = pair[1].split("\r\n")
+                statement = pair[1].strip().split("\r\n")
                 minute.append({"speaker":speaker, "statement":statement})
+
             # それ以外
             else:
                 line = line.strip()
+                print(f"（...）それ以外　［{line}］")
                 statement = line.split("\r\n")
                 minute.append({"speaker":"", "statement":statement})
 
