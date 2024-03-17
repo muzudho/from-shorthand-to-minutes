49c1a22375f00735995faa472f4f6b2829bfb3ee
むずでょ <muzudho1@gmail.com>
2024-03-17 14:56:55 +0900

    parse

--- shorthand_to_work.py
+++ shorthand_to_work.py
@@ -1,5 +1,6 @@
 # python shorthand_to_work.py
 import json
+import re
 
 def main():
     # 議事録
@@ -8,23 +9,34 @@ def main():
     with open("input.txt", "r", encoding="UTF-8") as f_in:
         lines = f_in.readlines()
         for line in lines:
-            #print(f"（＾～＾） {line}")
+            print(f"（＾～＾）　原文　［{line}］")
 
             # 末尾の改行は削除
-            # めんどくさいんでタブはざっくりスペース 4つに変換
-            line = line.rstrip().replace("\t", "    ")
+            # めんどくさいんでタブはざっくりスペース 1つに変換
+            line = line.rstrip().replace("\t", ' ')
+            print(f"（＾～＾）　タブをスペースへ変換　［{line}］")
+
+            # 連続したスペースがあれば１つにまとめたい
+            line = re.sub(r"\s+", ' ', line)
+            print(f"（＾～＾）　連続したスペースを１つに変換　［{line}］")
 
             # 空行はパス
             if line == "":
                 pass
+
             # 発言者名と発言の行
             elif not line.startswith(" "):
                 print(f"（＾～＾）発言者名と発言の行　［{line}］")
                 pair = line.split(" ", maxsplit=2)
                 speaker = pair[0]
                 print(f"（＾～＾）発言者名　［{speaker}］")
+                print(f"（＾～＾）残り　　　［{pair[1]}］")
                 # 改行は \r\n と仮定
                 statement = pair[1].strip().split("\r\n")
+
+                for statement_line in statement:
+                    print(f"(SL ) ［{statement_line}］")
+
                 minute.append({"speaker":speaker, "statement":statement})
 
             # それ以外
@@ -36,7 +48,7 @@ def main():
 
 
     with open("work.json", "w", encoding="UTF-8") as f_out:
-        json.dump(minute, f_out)
+        json.dump(minute, f_out, ensure_ascii=False)
 
 if __name__ == "__main__":
     main()
