c690409baf56ef9646a784eefc1374fe3836ba84
むずでょ <muzudho1@gmail.com>
2024-03-17 13:13:13 +0900

    array

--- shorthand_to_work.py
+++ shorthand_to_work.py
@@ -1,13 +1,16 @@
 # python shorthand_to_work.py
 
 def main():
+    array = []
+
     with open("input.txt", "r", encoding="UTF-8") as f:
         lines = f.readlines()
         for line in lines:
-            print(f"（＾～＾） {line}")
+            #print(f"（＾～＾） {line}")
+            array.append(line)
 
     with open("work.txt", "a", encoding="UTF-8") as f2:
-        for line in lines:
+        for line in array:
             f2.write(f"（＾～＾） {line}")
 
 if __name__ == "__main__":
