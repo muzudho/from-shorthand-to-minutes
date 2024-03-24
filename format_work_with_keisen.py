8024f865d51e91432051ebac071f763bc5754036
むずでょ <muzudho1@gmail.com>
2024-03-24 15:37:54 +0900

    Update format_work_with_keisen.py

--- format_work_with_keisen.py
+++ format_work_with_keisen.py
@@ -16,24 +16,23 @@ def main():
         for record in document:
             speaker = record["speaker"]
             statement = record["statement"]
-            is_first = True
 
-            for statement_line in statement:
-                #print(f"(...) {json.dumps(statement_line, ensure_ascii=False)}")
+            #print(f"(...) {json.dumps(statement, ensure_ascii=False)}")
 
-                # 発言者が変わる時、１行開ける
-                if is_first and speaker != "" and previous_speaker != speaker:
-                    print(f"(^q^) speaker：［{speaker}］　previous_speaker：［{previous_speaker}］　発言者が変わる時、１行開ける")
-                    f_out.write("　　　　　　　　　　　　｜\n")
-                else:
-                    print(f"(^q^) speaker：［{speaker}］　previous_speaker：［{previous_speaker}］　発言者同じ")
+            # 発言者が変わる時、１行開ける
+            if speaker != "" and previous_speaker != speaker:
+                print(f"(^q^) speaker：［{speaker}］　previous_speaker：［{previous_speaker}］　発言者が変わる時、１行開ける")
+                f_out.write("　　　　　　　　　　　　｜\n")
 
-                if is_first:
-                    #print("(^q^) 発言の１行目：［{statement_line}］")
-                    is_first = False
-                    f_out.write(f"　　　　　　　　　{speaker}　｜　{statement_line}\n")
-                else:
-                    f_out.write(f"　　　　　　　　　　　　｜　{statement_line}\n")
+            else:
+                print(f"(^q^) speaker：［{speaker}］　previous_speaker：［{previous_speaker}］　発言者同じ")
+
+            # 発言者名が入っているとき
+            if speaker != "":
+                #print("(^q^) 発言の１行目：［{statement}］")
+                f_out.write(f"　　　　　　　　　{speaker}　｜　{statement}\n")
+            else:
+                f_out.write(f"　　　　　　　　　　　　｜　{statement}\n")
 
             previous_speaker = speaker
 
